f535f632eb3cdc1731aed335b00486e45ec1d13a
Martin Wohlfart <github@wohlf.art>
2021-12-04 16:30:24 +0100

    Day 4

--- util/util.go
+++ util/util.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"runtime/pprof"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -14,6 +15,35 @@ var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 func ReadInput(in, splitBy string) []string {
 	trimmed := strings.Trim(in, "\n")
 	return strings.Split(trimmed, splitBy)
+}
+
+func NumberList(in string, separator string) []int {
+	var list []int
+	for _, s := range strings.Split(in, separator) {
+		if s == "" {
+			continue
+		}
+		list = append(list, Str2Int(s))
+	}
+
+	return list
+}
+
+func Str2Int(in string) int {
+	i, _ := strconv.Atoi(in)
+	return i
+}
+
+func NewBoolMatrix(width, height int) map[int]map[int]bool {
+	m := make(map[int]map[int]bool, width)
+	for i := 0; i < width; i++ {
+		m[i] = make(map[int]bool, height)
+		for j := 0; j < height; j++ {
+			m[i][j] = false
+		}
+	}
+
+	return m
 }
 
 func WithTime() func() {
