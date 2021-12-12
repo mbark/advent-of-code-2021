9ec22820d9968ecb32ac9550caab66ec8aef10d0
Martin Wohlfart <github@wohlf.art>
2021-12-12 13:51:23 +0100

    Optimize day 12

--- day12/main.go
+++ day12/main.go
@@ -63,22 +63,42 @@ MV-start
 DD-ko
 `
 
-var (
-	startNode = Node("start")
-	endNode   = Node("end")
-)
-
 func main() {
 	g := make(Graph)
+	nodeMap := make(map[string]int)
+	n := 1
+
+	var start Node
 	for _, l := range util.ReadInput(in, "\n") {
 		s := strings.Split(l, "-")
-		g[Node(s[0])] = append(g[Node(s[0])], Node(s[1]))
-		g[Node(s[1])] = append(g[Node(s[1])], Node(s[0]))
+		n1, n2 := nodeMap[s[0]], nodeMap[s[1]]
+		if n1 == 0 {
+			n1 = n
+			n++
+			nodeMap[s[0]] = n1
+		}
+		if n2 == 0 {
+			n2 = n
+			n++
+			nodeMap[s[1]] = n2
+		}
+
+		node1 := Node{n: n1, s: s[0]}
+		node2 := Node{n: n2, s: s[1]}
+
+		g[node1] = append(g[node1], node2)
+		g[node2] = append(g[node2], node1)
+
+		if node1.IsStart() {
+			start = node1
+		}
+		if node2.IsStart() {
+			start = node2
+		}
 	}
 
-	fmt.Println(g)
-	fmt.Printf("first: %d\n", solve(g, 1))
-	fmt.Printf("first: %d\n", solve(g, 2))
+	fmt.Printf("first: %d\n", solve(g, start, 1))
+	fmt.Printf("first: %d\n", solve(g, start, 2))
 }
 
 type Graph map[Node][]Node
@@ -87,23 +107,41 @@ func (g Graph) Next(n Node) []Node {
 	return g[n]
 }
 
-type Node string
+type Node struct {
+	n int
+	s string
+}
 
 func (n Node) IsBig() bool {
-	return strings.ToUpper(string(n)) == string(n)
+	return strings.ToUpper(n.s) == n.s
+}
+
+func (n Node) IsStart() bool {
+	return n.s == "start"
+}
+
+func (n Node) IsEnd() bool {
+	return n.s == "end"
 }
 
 type Visited struct {
 	max int
-	m   map[Node]int
+	m   []int
 }
 
-func NewVisited(max int) Visited {
-	return Visited{max: max, m: make(map[Node]int)}
+func NewVisited(max int, graph Graph) Visited {
+	var maxVal int
+	for n := range graph {
+		if n.n > maxVal {
+			maxVal = n.n
+		}
+	}
+
+	return Visited{max: max, m: make([]int, maxVal)}
 }
 
 func (v Visited) Add(n Node) {
-	v.m[n] += 1
+	v.m[n.n] += 1
 }
 
 func (v Visited) Visited(n Node) bool {
@@ -111,12 +149,12 @@ func (v Visited) Visited(n Node) bool {
 		return false
 	}
 
-	val := v.m[n]
+	val := v.m[n.n]
 	if val == 0 {
 		return false
 	}
 
-	if val > 0 && (n == startNode || n == endNode) {
+	if val > 0 && (n.IsStart() || n.IsEnd()) {
 		return true
 	}
 
@@ -135,7 +173,7 @@ func (v Visited) Visited(n Node) bool {
 }
 
 func (v Visited) Copy() Visited {
-	cp := make(map[Node]int, len(v.m))
+	cp := make([]int, len(v.m))
 	for k, v := range v.m {
 		cp[k] = v
 	}
@@ -143,37 +181,30 @@ func (v Visited) Copy() Visited {
 	return Visited{m: cp, max: v.max}
 }
 
-func solve(graph Graph, max int) int {
-	paths := recurse(NewVisited(max), startNode, graph)
-	for _, p := range paths {
-		for i, j := 0, len(p)-1; i < j; i, j = i+1, j-1 {
-			p[i], p[j] = p[j], p[i]
-		}
-	}
-	return len(paths)
+func solve(graph Graph, start Node, max int) int {
+	paths := recurse(NewVisited(max, graph), start, graph)
+	return paths
 }
 
-func recurse(visited Visited, node Node, graph Graph) [][]Node {
-	if node == endNode {
-		return [][]Node{{node}}
-	}
-
-	if visited.Visited(node) {
-		return nil
-	}
-
+func recurse(visited Visited, node Node, graph Graph) int {
 	if !node.IsBig() {
+		visited = visited.Copy()
 		visited.Add(node)
 	}
-	var paths [][]Node
+
+	var count int
 	for _, next := range graph.Next(node) {
-		ps := recurse(visited.Copy(), next, graph)
-		if ps != nil {
-			for _, p := range ps {
-				paths = append(paths, append(p, node))
-			}
+		if next.IsEnd() {
+			count += 1
+			continue
 		}
+
+		if visited.Visited(next) {
+			continue
+		}
+
+		count += recurse(visited, next, graph)
 	}
 
-	return paths
+	return count
 }
